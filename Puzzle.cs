4bdd946616c9ea556a6d83ddff6d49b19eca9a7a
Aidan Ferry <aidan.ferry@ametek.com>
2026-04-30 16:02:17 -0400

    Migrated to .NET 5

--- Puzzle.cs
+++ Puzzle.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows.Forms;
@@ -123,6 +124,7 @@ namespace CrosswordApp
                     {
                         sel.row=0;
                         sel.col++;
+                        if (sel.col >= xwd.getWidth()) sel.col = 0;
                     }
                     else
                     {
@@ -139,6 +141,7 @@ namespace CrosswordApp
                     {
                         sel.col = 0;
                         sel.row++;
+                        if (sel.row >= xwd.getWidth()) sel.row = 0;
                     }
                     else
                     {
--- XWDApp.cs
+++ XWDApp.cs
@@ -1,3 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace CrosswordApp
 {
     public enum Tab
